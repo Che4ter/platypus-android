a37143dfa0caa494238c7fd5510ddf89a41b6ec1
Che4ter <che4ter@42.wtf>
2017-05-10 17:25:02 +0200

    Initial commit

---
+++ app/src/main/java/ch/stair/platypus/CardContentFragment.java
@@ -0,0 +1,142 @@
+package ch.stair.platypus;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+/**
+ * Provides UI for the view with Cards.
+ */
+public class CardContentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public CardView card;
+        public TextView name;
+        public TextView description;
+
+        private int randColor(Context context)
+        {
+            List<Integer> colorList = new ArrayList<Integer>();
+
+            int[] colors = context.getResources().getIntArray(R.array.card_backgrounds);
+
+            int rn = new Random().nextInt(colors.length - 1);
+            return colors[rn];
+        }
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_card, parent, false));
+            name = (TextView) itemView.findViewById(R.id.card_title);
+            description = (TextView) itemView.findViewById(R.id.card_text);
+            card = (CardView) itemView.findViewById(R.id.card_view);
+
+            card.setCardBackgroundColor(randColor(parent.getContext()));
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, DetailActivity.class);
+                    intent.putExtra(DetailActivity.EXTRA_POSITION, getAdapterPosition());
+                    context.startActivity(intent);
+                }
+            });
+
+            // Adding Snackbar to Action Button inside card
+            Button button = (Button)itemView.findViewById(R.id.action_button);
+            button.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, "Action is pressed",
+                            Snackbar.LENGTH_LONG).show();
+                }
+            });
+
+            ImageButton favoriteImageButton =
+                    (ImageButton) itemView.findViewById(R.id.favorite_button);
+            favoriteImageButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, "Added to Favorite",
+                            Snackbar.LENGTH_LONG).show();
+                }
+            });
+
+            ImageButton shareImageButton = (ImageButton) itemView.findViewById(R.id.reply_button);
+            shareImageButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, "Share article",
+                            Snackbar.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    /**
+     * Adapter to display recycler view.
+     */
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of Card in RecyclerView.
+        private static final int LENGTH = 18;
+
+        private final String[] mPlaces;
+        private final String[] mPlaceDesc;
+
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mPlaces = resources.getStringArray(R.array.places);
+            mPlaceDesc = resources.getStringArray(R.array.place_desc);
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.name.setText(mPlaces[position % mPlaces.length]);
+            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
+    }
+}
